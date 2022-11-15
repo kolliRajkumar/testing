b5c154f6c8685c254a9c5ea4726074a1b5601ee9
kolliRajkumar <101019188+kolliRajkumar@users.noreply.github.com>
2022-11-15 12:12:11 +0530

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,11 +12,3 @@ df = pd.DataFrame({
     'second column': [10,20,30,40]
     })
 st.write(df)
-
-
-df2 = pd.DataFrame(
-     np.random.randn(200, 3),
-     columns=['a', 'b', 'c'])
-c = alt.Chart(df2).mark_circle().encode(
-     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
-st.write(c)
